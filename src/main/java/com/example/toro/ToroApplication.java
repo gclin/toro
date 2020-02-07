fcdb29aa4c12916670c5e66e465466e8abb0198d
Austin <austin@gclin.org>
2020-02-07 12:25:05 +0800

    Create spring boot project with Spring Boot DevTools, Lombok, Spring Configuration Processor, Spring Web and Thymeleaf.

---
+++ src/main/java/com/example/toro/ToroApplication.java
@@ -0,0 +1,13 @@
+package com.example.toro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toro/ToroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
